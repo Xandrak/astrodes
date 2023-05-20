94ac1e6e46c0216d404ee7005bd77bec27ba8a19
Xandrak <dustin.segers@gmail.com>
2023-05-20 14:53:38 -0500

    fix webpack for dev and building; remove babel; add ts-loader

--- webpack/base.js
+++ webpack/base.js
@@ -4,37 +4,41 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
+  entry: "./src/index.ts",
   mode: "development",
   devtool: "eval-source-map",
   module: {
     rules: [
       {
-        test: /\.js$/,
+        test: /\.tsx?$/,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader"
-        }
+          loader: "ts-loader",
+        },
       },
       {
         test: [/\.vert$/, /\.frag$/],
-        use: "raw-loader"
+        use: "raw-loader",
       },
       {
         test: /\.(gif|png|jpe?g|svg|xml)$/i,
-        use: "file-loader"
-      }
-    ]
+        use: "file-loader",
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
   },
   plugins: [
     new CleanWebpackPlugin({
-      root: path.resolve(__dirname, "../")
+      root: path.resolve(__dirname, "../"),
     }),
     new webpack.DefinePlugin({
       CANVAS_RENDERER: JSON.stringify(true),
-      WEBGL_RENDERER: JSON.stringify(true)
+      WEBGL_RENDERER: JSON.stringify(true),
     }),
     new HtmlWebpackPlugin({
-      template: "./index.html"
-    })
-  ]
+      template: "./index.html",
+    }),
+  ],
 };
--- webpack/prod.js
+++ webpack/prod.js
@@ -4,24 +4,47 @@ const base = require("./base");
 const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = merge(base, {
+  entry: "./src/index.ts",
   mode: "production",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "ts-loader",
+        },
+      },
+      {
+        test: [/\.vert$/, /\.frag$/],
+        use: "raw-loader",
+      },
+      {
+        test: /\.(gif|png|jpe?g|svg|xml)$/i,
+        use: "file-loader",
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
   output: {
-    filename: "bundle.min.js"
+    filename: "bundle.min.js",
   },
   devtool: false,
   performance: {
     maxEntrypointSize: 900000,
-    maxAssetSize: 900000
+    maxAssetSize: 900000,
   },
   optimization: {
     minimizer: [
       new TerserPlugin({
         terserOptions: {
           output: {
-            comments: false
-          }
-        }
-      })
-    ]
-  }
+            comments: false,
+          },
+        },
+      }),
+    ],
+  },
 });
